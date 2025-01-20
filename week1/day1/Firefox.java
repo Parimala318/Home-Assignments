7d52ad85ae4b2614cdfac361e2768e4709a3a812
Parimala318 <pari318@gmail.com>
2025-01-20 23:59:51 +0530

    Add files via upload
    Week 1

---
+++ week1/day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion=100.2F;
+		System.out.println(browserVersion);
+		String browserName="firefox";
+		System.out.println(browserName);
+		Boolean isVisible=true;
+		System.out.println(isVisible);
+		int releaseYear=1998;
+		System.out.println(releaseYear);
+		char browserLogo='f';
+		System.out.println(browserLogo);
+		
+
+	}
+
+}
---
+++ week1/day1/LearnDataTypes.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	int age=25;
+	
+	public static void main(String[] args)
+	{
+		
+		
+		byte age=25;
+		short depositAmount=30000;
+		int deposit=12000;
+		long mobileNumber=984037804;
+		float roi=9.9F;
+		float amount1=0.3423325235234F;
+		System.out.println(amount1);
+		double amount2=0.32423423234235;
+		System.out.println(amount2);
+		char logo='X';
+		System.out.println(logo);
+		char ascii=65;
+		System.out.println(ascii);
+		boolean doYouknowJava=true;
+		System.out.println(doYouknowJava);
+	}
+
+}
